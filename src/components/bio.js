6182c464ab373cfae74f96d2c9c62ea211bff895
Mutsuki Kimuraya <zaubermaerchen@me.com>
2022-07-28 13:10:13 +0900

    fix

--- src/components/bio.js
+++ src/components/bio.js
@@ -7,7 +7,6 @@
 
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
@@ -32,23 +31,15 @@ const Bio = () => {
 
   return (
     <div className="bio">
-      <StaticImage
-        className="bio-avatar"
-        layout="fixed"
-        formats={["auto", "webp", "avif"]}
-        src="../images/profile-pic.png"
-        width={50}
-        height={50}
-        quality={95}
-        alt="Profile picture"
-      />
       {author?.name && (
         <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
+          Written by
           {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
-          </a>
+          <strong>
+            <a href={`https://twitter.com/${social?.twitter || ``}`}>
+              {author.name}
+            </a>
+          </strong>
         </p>
       )}
     </div>
